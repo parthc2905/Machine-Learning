b5a6d794a17a888500ec1b526572c0447c966f6a
parthc2905 <chauhanp2905@gmail.com>
2026-03-03 00:07:51 +0530

    Refactor code structure for improved readability and maintainability

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
+#fetch data from a website
 import requests
-from bs4 import BeautifulSoup
-
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
 agent = {"User-Agent":'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
-url = "https://www.ambitionbox.com/list-of-companies?campaign=desktop_nav&page=500"
-
-response = requests.get(url,headers=agent)
-# print(response.text)
-soup = BeautifulSoup(response.text , 'lxml')
-mydivs = soup.find_all('div', class_="companyCardWrapper")
-import os
-print(mydivs)
-os.system('cls')
+url = "https://www.youtube.com/watch?v=4HyTlbHUKSw&list=PLKnIA16_Rmvbr7zKYQuBfsVkjoLcJgxHH&index=24"
+response = requests.get(url,headers=headers) # getting data
+print(response.text)

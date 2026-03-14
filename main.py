8c2f9780c5eceb97336a41011ec9629b8cccbec0
parthc2905 <chauhanp2905@gmail.com>
2026-03-14 18:42:34 +0530

    Add pipeline for preprocessing and model training
    - Implemented a pipeline using sklearn's Pipeline and ColumnTransformer.
    - Added SimpleImputer for handling missing values in 'Age' and 'Embarked' columns.
    - Included OneHotEncoder for categorical features 'Sex' and 'Embarked'.
    - Integrated MinMaxScaler for feature scaling.
    - Utilized SelectKBest for feature selection based on chi-squared statistics.
    - Incorporated DecisionTreeClassifier for model training.

--- main.py
+++
@@ -1,7 +0,0 @@
-#fetch data from a website
-import requests
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
-agent = {"User-Agent":'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
-url = "https://www.youtube.com/watch?v=4HyTlbHUKSw&list=PLKnIA16_Rmvbr7zKYQuBfsVkjoLcJgxHH&index=24"
-response = requests.get(url,headers=headers) # getting data
-print(response.text)
